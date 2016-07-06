a202a46eac9a4ce7c113e3551413ee12d2ae97b5
tfolly <tfolly@student.42.fr>
2016-07-06 16:33:50 +0200

    ok mtn ca remarche

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: tfolly <tfolly@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/27 16:39:28 by tfolly            #+#    #+#             */
-/*   Updated: 2016/07/06 16:09:12 by tfolly           ###   ########.fr       */
+/*   Updated: 2016/07/06 16:30:06 by tfolly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,5 +50,10 @@ int main(int ac, char **av)
 		file = ft_file_list(av[2], opt);
 	}
 	ft_print_all_files(file, opt);
+	// while (file)
+	// {
+	// 	ft_printf("%s\n", file->name);
+	// 	file = file->next;
+	// }
 	return (0);
 }
